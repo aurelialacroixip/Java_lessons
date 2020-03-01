14417787b1c8058922f2de93110d9fd2a5f1fadc
Albina Sorokina <albina-sorokina@bk.ru>
2020-03-01 19:38:31 +0700

    Calculator with main.java

--- MyCalc/src/com/company/Calc.java
+++ MyCalc/src/com/company/Calc.java
@@ -3,13 +3,22 @@ package com.company;
 import java.util.Scanner;
 
 class Calc {
-    public static final void main(String[] args) {
-        Integer a;
-        Integer b;
+    private double a;
+    private double b;
+    private Character op;
+
+    public Calc () {
+        this.a = 0.0;
+        this.b = 0.0;
+        this.op = ' ';
+    }
+    public static final void Calculator() {
+        double a;
+        double b;
         Character op;
         Scanner in = new Scanner(System.in);
-        a = in.nextInt();
-        b = in.nextInt();
+        a = in.nextDouble();
+        b = in.nextDouble();
         op = in.next().charAt(0);
         switch(op) {
             case '+':{
--- MyCalc/src/com/company/Main.java
+++ MyCalc/src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Calc ca = new Calc();
+	    ca.Calculator();
     }
 }
 
