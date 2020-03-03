cd372345f61dd605cd3937bdecac62383f5184ec
Albina Sorokina <albina-sorokina@bk.ru>
2020-03-03 16:55:09 +0700

    App with right input

--- ClassTree/src/main/java/com/company/Main.java
+++ ClassTree/src/main/java/com/company/Main.java
@@ -6,23 +6,59 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code ]
+
+        Character ch; //for choosing: stud or teach
+
         Integer ag;
         String nam;
         Character s;
         String gr;
         Float exp;
 
+        System.out.println("Enter 0 to enter data for student or Enter 1 for teacher");
+
         Scanner in = new Scanner(System.in);
-        ag = in.nextInt();
+        ch = in.next().charAt(0);
+
+        switch (ch) {
+            case '0':{
+                System.out.println("Enter age of stud");
+                ag = in.nextInt();
+                System.out.println("Enter name of stud");
+                nam = in.next();
+                System.out.println("Enter sex of stud");
+                s = in.next().charAt(0);
+                System.out.println("Enter group");
+                gr = in.next();
+
+                Stud st = new Stud(ag, nam, s, gr);
+                st.printall();
+            }
+            break;
+            case '1':{
+                System.out.println("Enter age of teach");
+                ag = in.nextInt();
+                System.out.println("Enter name of teach");
+                nam = in.next();
+                System.out.println("Enter sex of teach");
+                s = in.next().charAt(0);
+                System.out.println("Enter experience");
+                exp = in.nextFloat();
+
+                Teach te = new Teach(ag, nam, s, exp);
+                te.printall();
+            }
+            break;
+        }
+
+        /*ag = in.nextInt();
         nam = in.next();
         s = in.next().charAt(0);
         gr = in.next();
-        exp = in.nextFloat();
+        exp = in.nextFloat();*/
 
-        Stud st = new Stud(ag, nam, s, gr);
-        st.printall();
 
-        Teach te = new Teach(ag, nam, s, exp);
-        te.printall();
+
+
     }
 }
