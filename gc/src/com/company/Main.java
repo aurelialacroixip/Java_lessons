0ffd8d354d8e998180dac8c355da2da0b70fbce2
Albina Sorokina <albina-sorokina@bk.ru>
2020-03-15 16:19:25 +0700

    creation of many objects

---
+++ gc/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int i;
+        Calc[] name = new Calc [100];
+        for (i = 0; i < 100; i++) {
+            name[i] = new Calc();
+        }
+    }
+}
